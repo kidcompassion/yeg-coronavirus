c1746386d25865a7a7969a966210d98f34c2c3ca
kidcompassion <sallypoulsen@gmail.com>
2020-03-14 16:44:52 -0600

    Resolves issue with layout breaking in mobile.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = () =>{
     return(
         <header>
-            <h1>Current <span>Google Alerts</span> for <span>"coronavirus"</span> and <span>"Edmonton"</span></h1>
+            <h1>Current <span>Google Alerts</span> for </h1>
+            <h1><span>"coronavirus"</span> and <span>"Edmonton"</span></h1>
             <p>Refresh for newest updates</p>
         </header>
     );
